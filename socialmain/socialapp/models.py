acd0584cd98377f283687975628e54bb04726156
Fayaz409 <ftunio06@gmail.com>
2024-08-27 13:26:40 +0500

    Gave the a better style and added the model Profile for Databse user creation

--- socialmain/socialapp/models.py
+++ socialmain/socialapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    profileimg=models.ImageField(upload_to='profile_images', )
+    email = models.EmailField()
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " " + str(self.age)
